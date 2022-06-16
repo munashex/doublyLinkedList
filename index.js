a50ab51e1431c6baf9ebdf5ae4631d42d2e31596
munashe <munashemugondaa@gmail.com>
2022-06-16 09:13:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+  }
+  
+  
+  class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+  
+    pop() {
+      if(!this.head) return null 
+      let poppedNode = this.tail 
+      if(this.length === 1) {
+          this.head = null 
+          this.tail = null
+      }else {
+       this.tail = poppedNode.prev 
+       this.tail.next = null 
+       poppedNode.prev = null
+      }
+      this.length-- 
+      return poppedNode
+  }
+  
+  }
+  
+  
+  let list = new DoublyLinkedList() 
+  list.push("munashe") 
+  list.push("madhuna")
+  list.push("shmurda")
+  list.pop()
+  console.log(list)
