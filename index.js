5e6bd5e823a09d48d9b6672773c4c840846a7035
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-06-20 09:26:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,22 @@ class Node{
    this.length-- 
    return newHead
  }
+      
+      
+ unshift(val) {
+   let newNode = new Node(val) 
+
+   if(!this.head){
+     this.head = newNode
+     this.tail = newNode
+   }else {
+   this.head.prev = newNode  
+   newNode.next = this.head 
+   this.head = newNode
+   }
+   this.length++ 
+   return this
+ }
   
   }
   
