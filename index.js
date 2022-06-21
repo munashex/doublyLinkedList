f32718503d47d4eb5fa4507f72197c417eced15b
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-06-21 10:27:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,27 @@ class Node{
    return this
  }
   
+       get(index){
+        if(index < 0 || index >= this.length) return null;
+        var count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+}
   }
   
   
