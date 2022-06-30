55c5391d3cb684b2db7c5a32fe41f55a63ecb4b0
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-06-30 09:00:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,23 @@ insert(index, val){
         this.length++;
         return true;
     }
+
+remove(index){
+     if(index < 0 || index >= this.length) return false 
+     if(index === 0) return this.shift() 
+     if(index === this.length - 1) return this.pop()
+
+     let removedNode = this.get(index) 
+     let beforeNode = removedNode.prev 
+     let afterNode = removedNode.next  
+     beforeNode.next = afterNode 
+     afterNode.prev = beforeNode
+
+     removedNode.next = null 
+     removedNode.prev = null
+     this.length--
+     return removedNode
+    }
   }
   
   
